f15ad0baa69437f4bdaff61ed0781adad9842db7
rbattala-rt <rbattala@randomtrees.com>
2025-04-03 15:00:58 +0530

    update app.py with models

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.sidebar:
     api_key = st.text_input("Enter API Key", type="password")
     model = st.selectbox(
         "Select Model",
-        ["llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"]
+        ["llama3-8b-8192", "llama3-70b-8192"]
     )
     
     #st.markdown("---")
